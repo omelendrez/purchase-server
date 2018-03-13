80cc8bb86fe7a295d45a89eec0d3042608a6a709
omelendrez <omar.melendrez@gmail.com>
2018-03-13 20:43:04 -0300

    Change date format for postgres

--- api/v1/lib/constants.js
+++ api/v1/lib/constants.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const DATE_FORMAT_FUNCTION = 'date_format'; // 'to_char'
-const DATE_FORMAT_PARAMS = '%d-%b-%y %H:%i' // 'DD-Mon-YY HH24:MI';
-const SHORT_DATE_FORMAT_PARAMS = '%d-%b-%y' // 'DD-Mon-YY';
-const DEFAULT_DATE_FORMAT_PARAMS = '%Y-%m-%d';
+const DATE_FORMAT_FUNCTION = 'to_char'; // 'date_format'; // 'to_char'
+const DATE_FORMAT_PARAMS = 'DD-Mon-YY HH24:MI'; // '%d-%b-%y %H:%i' // 'DD-Mon-YY HH24:MI';
+const SHORT_DATE_FORMAT_PARAMS = 'DD-Mon-YY' // '%d-%b-%y' // 'DD-Mon-YY';
+const DEFAULT_DATE_FORMAT_PARAMS = 'YYYY-MM-DD' ;// '%Y-%m-%d';
 const UPPER = 'upper';
 const DEFAULT_PASSWORD = 'purchase+'
 const errorMessage = [
