cc000f2771d3c42e29e1ddfd08489336070b6cbd
omelendrez <omar.melendrez@gmail.com>
2018-04-09 15:24:07 -0300

    Remarks in document status shall be text type not string

--- api/v1/models/document_status.js
+++ api/v1/models/document_status.js
@@ -18,7 +18,7 @@ module.exports = function (sequelize, DataTypes) {
       allowNull: false
     },
     remarks: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: false
     }
   });
